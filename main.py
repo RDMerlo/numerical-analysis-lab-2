cec6236c9c535dc838854e993dea837b58a040ed
RDMerlo <68595137+RDMerlo@users.noreply.github.com>
2022-11-22 20:14:09 +0000

    Сделал пункты 2-4

--- main.py
+++ main.py
@@ -6,22 +6,15 @@ from numpy.core.fromnumeric import partition
 
 n = 4
 
-#смена колонок
-def swap_columns(a, i, j):
-  for k in range(len(a)):
-    a[k][i], a[k][j] = a[k][j], a[k][i]
-
-#смена строк
-def swap_row(a, i, j):
-  temp = copy.copy(A[i])
-  A[i] = A[j]
-  A[j] = temp
-
-def discrepancy(matrix,vector,vector1): #невязка
-  for i in range(n):
-    vector1[i] = matrix[i][-1] - sum([matrix[i][j] * vector[j] for j in range(0, 4)])
-
-def norm(matrix):  #норма матрицы 
+#вектор невязки
+# def get_residual_vector(matrix, X):
+#   discrepancy = np.zeros((4, 1))
+#   for i in range(n):
+#     discrepancy[i] = matrix[i][-1] - sum([matrix[i][j] * X[j] for j in range(0, 4)])
+#   return discrepancy
+  
+#норма матрицы ||A||_1
+def get_norm_matrix(matrix): 
   x = np.zeros((n, 1))
   for i in range(n):
       x[i]=sum(abs(matrix[i]))
@@ -83,31 +76,42 @@ def get_Xi(C, X, Y, i):
       sum += C[i][k]*X[k]
     return Y[i] - sum
 
+def gauss_seidel_method(A, D, eps = 1e-6):
+  X = np.zeros(n)
+  stop_criterion = False
+  while not stop_criterion:
+    x_new = np.copy(X)
+    for i in range(n):
+      s1 = sum(A[i][j] * x_new[j] for j in range(i))
+      s2 = sum(A[i][j] * X[j] for j in range(i + 1, n))
+      x_new[i] = (D[i] - s1 - s2) / (A[i][i])
+    vector_norm = LA.norm(x_new[i] - X[i]) #норма ||.||_2
+    stop_criterion = vector_norm < eps
+    X = x_new
+  return X
+
 A_orig = np.array([(10, -1, -2,  5), 
                    (-1, 12,  3, -4),
                    (-2, 3,  15,  8), 
                    (5, -4,  8,  18)])
+
 print("Исходная матрица:")
 print_array(A_orig)
 A = copy.copy(A_orig)
 
 D = np.array([95, -41, 69, 27])
 
-# B = np.array([(0, 0, 0,  0), 
-#               (0, 0, 0,  0),
-#               (0, 0, 0,  0), 
-#               (0, 0, 0,  0)])
-
-# C = np.array([(0, 0, 0,  0), 
-#               (0, 0, 0,  0),
-#               (0, 0, 0,  0), 
-#               (0, 0, 0,  0)])
-
 B = np.zeros((n,n))
 C = np.zeros((n,n))
 
 Y = np.array([None, None, None, None])
 X = np.array([None, None, None, None])
+
+# A_Extended = np.column_stack((A, D))
+# discrepancy = get_residual_vector(A_Extended, X)
+# for i in range (n):
+#   print(f"r{i+1}=%.16f"% (abs(discrepancy[i])))
+
 
 for i in range(0, n, 1):
   for j in range(0, i+1, 1):
@@ -130,3 +134,19 @@ print("Y: ")
 print_vector(Y)
 print("X: ")
 print_vector(X)
+
+print("\nВектор невязки")
+r = np.dot(A, X) - D
+print_vector_18(r)
+
+print("\nМетод Гаусса — Зейделя")
+X_2 = gauss_seidel_method(A, D)
+print_vector(X_2)
+
+print("\nЧисло обусловленности")
+norm1=get_norm_matrix(A)
+norm2=get_norm_matrix(np.linalg.inv(A))
+print("%2.6f" %(norm1*norm2))
+
+print("\nЧисло обусловленности - питон")
+print("%2.6f" % (LA.cond(A, np.inf)))
